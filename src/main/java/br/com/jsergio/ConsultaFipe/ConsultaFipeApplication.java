096521aaa23846aa703ccccfc3e5970424cf71c9
José Sérgio <Maverikxd1@gmail.com>
2024-05-04 15:13:23 -0300

    first commit

---
+++ src/main/java/br/com/jsergio/ConsultaFipe/ConsultaFipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsergio.ConsultaFipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jsergio/ConsultaFipe/ConsultaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jsergio.ConsultaFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
